88739455c1e3b879f82626bbfd1e7f0ceeb6c29c
Md. Sazim Mahmudur Rahman <sazim87@student.sust.edu>
2025-11-15 19:21:34 +0600

    Chore: Add credential storage of admin & supplier

---
+++ admin_details.php
@@ -0,0 +1,13 @@
+<?php 
+$admin_account_number = "5151";
+$admin_email = "admin@gmail.com";
+$admin_name = "Sylhet IT Shop Owner";
+$admin_secret = "ADMIN_SECRET";
+$admin_password = "ADMIN_PASSWORD";
+
+$supplier_account_number = "30593";
+$supplier_email = "techwholesaler@gmail.com";
+$supplier_password = "SUPPLIER_PASSWORD";
+
+$apikey = "SENDGRID_API";
+?>
